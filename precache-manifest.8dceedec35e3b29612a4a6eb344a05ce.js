b384bd89109146d9d6a331263fa8ffad2c68382b
Lior Sasson <Lior@Liors-Mac-mini.lan>
2020-04-25 01:23:37 +0300

    Updates

---
+++ precache-manifest.8dceedec35e3b29612a4a6eb344a05ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4d3c3fd7a033a9ee1d6112ed99c42ad",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "26a7f3e879ef7cf037ed",
+    "url": "/monsters-rolodex/static/css/main.6168ae22.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26a7f3e879ef7cf037ed",
+    "url": "/monsters-rolodex/static/js/main.bda2cbb0.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
